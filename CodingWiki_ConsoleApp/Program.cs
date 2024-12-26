5a422231d733b41ed0da509702cd1daecbe6e848
Amir Mosallanejad <124431071+Amir-Mosallanejad@users.noreply.github.com>
2024-12-26 18:04:46 +0330

    CW 1 Create ConsoleApp Project
    add database Check and add migration automatically

--- CodingWiki_ConsoleApp/Program.cs
+++ CodingWiki_ConsoleApp/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+await using ApplicationDbContext context = new ApplicationDbContext();
+await context.Database.EnsureCreatedAsync();
+if (context.Database.GetPendingMigrations().Any())
+{
+    await context.Database.MigrateAsync();
+}
