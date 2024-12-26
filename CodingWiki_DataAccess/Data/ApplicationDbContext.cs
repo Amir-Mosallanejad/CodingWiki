ebd9affe5ceafeb6bbe6c7d0e7816a6e73757781
Amir Mosallanejad <124431071+Amir-Mosallanejad@users.noreply.github.com>
2024-12-26 20:19:54 +0330

    CW 4 Add Logger For Context

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -1,18 +1,21 @@
 ﻿using CodingWiki_DataAccess.FluentConfig;
 using CodingWiki_Model.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CodingWiki_DataAccess.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=.;Database=CodingWikiDb;Trusted_Connection=True;");
-    }
-
     public DbSet<Book> Books { get; set; }
     public DbSet<BookDetail> BookDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer("Server=.;Database=CodingWikiDb;Trusted_Connection=True;")
+            .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name }, LogLevel.Information);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
