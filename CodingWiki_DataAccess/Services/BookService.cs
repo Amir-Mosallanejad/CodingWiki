7ee1bb58ba53955842973b6c254475ec659ee996
Amir Mosallanejad <124431071+Amir-Mosallanejad@users.noreply.github.com>
2024-12-27 15:34:02 +0330

    CW 6 use entry reference load to make query more effeicnet

---
+++ CodingWiki_DataAccess/Services/BookService.cs
@@ -0,0 +1,19 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingWiki_DataAccess.Services;
+
+public class BookService(ApplicationDbContext context)
+{
+    public async Task<List<Book>> ReturnBookDetailFromBooks()
+    {
+        List<Book> book = await context.Books.ToListAsync();
+        foreach (var obj in book)
+        {
+            await context.Entry(obj).Reference(u => u.BookDetail).LoadAsync();
+        }
+
+        return book;
+    }
+}
